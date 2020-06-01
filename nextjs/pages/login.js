dce5debe30615fcdd43186fc5798feb77b4971f9
erik villa <erik@wizeline.com>
2020-06-01 14:31:59 -0500

    debug: Add log

--- nextjs/pages/login.js
+++ nextjs/pages/login.js
@@ -21,7 +21,9 @@ const Login = () => {
     }
 
     try {
+      console.log(`public key: ${process.env.MAGIC_PUBLISHABLE_KEY}`)
       const magic = new Magic(process.env.MAGIC_PUBLISHABLE_KEY)
+      
       const didToken = await magic.auth.loginWithMagicLink({
         email: body.email,
       })
